896df9f42f1329831a473ce1c806348e1464c16e
Ze Xian Choo <99162756+zexianchoo@users.noreply.github.com>
2023-10-18 17:41:51 -0500

    done with home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,21 +7,25 @@ const Hero = () => {
   return (
     <section
       id='home'
-      className='w-full flex justify-center justify-items-center text-center min-h-screen max-container fading-down-slow'
+      className='pt-64 flex justify-between items-center max-container min-h-full'
     >
-      <div className='relative w-half pt-48'>
+      <div className='relative fading-down-slow'>
         <img
           src={ face }
           alt='picture'
-          width={300}
-          height={300}
+          width={1200}
+          height={1200}
           className='rounded-full'
         />
-        <p className='text-3xl font-montserrat text-dark-blue py-8'>
+        <p className='relative text-3xl text-center font-montserrat text-dark-blue py-8 fading-down-slow'>
           Choo Ze Xian
         </p>
-
-      </div>
+        </div>
+        <div className='relative'>
+            <p className='text-xl font-montserrat text-dark-blue pl-32 fading-right-slower' >
+            Hi! I'm a sophomore at the University of Illinois Urbana-Champaign studying Computer Science. I am a software developer interested in developing applications that use Artificial Intelligence, Machine Learning, and Computer Vision. I have experience working Pytorch, Tensorflow, OpenCV and React. I am passionate about developing solutions that provide business value, and am constantly looking for oppourtunities to improve my skills as a Software Engineer.
+            </p>
+        </div>
     </section>
   );
 }

342c4edee6cfe7cc2b41b9cc0a61e4fc6a7711c9
Ze Xian Choo <99162756+zexianchoo@users.noreply.github.com>
2023-10-18 21:10:37 -0500

    home done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section
       id='home'
-      className='relative pt-36 max-sm:pt-8 sm:flex justify-between items-center max-container min-h-full'
+      className='relative h-[60vh] max-sm:pt-8 sm:flex justify-between items-center max-container px-8  min-h-full'
     >
       <div className='sm:relative max-sm:text-lg fading-down-slow'>
         <img
@@ -18,10 +18,10 @@ const Hero = () => {
           className='rounded-full'
         />
         <div>
-        <p className='relative max-sm:text-2xl font-semibold first-line:text-3xl text-center font-montserrat text-dark-blue max-sm:py-2 min-sm:py-8 fading-down-slow'>
+        <p className='relative max-sm:text-2xl font-semibold sm:text-3xl text-center font-montserrat text-dark-blue max-sm:py-2 min-sm:py-8 fading-down-slow'>
           Choo Ze Xian
         </p>
-        <p className='relative max-sm:text-2xl text-2xl text-center font-montserrat text-dark-blue max-sm:py-2 min-sm:py-8 fading-down-slow'>
+        <p className='relative max-sm:text-lg max-md:xl md:text-2xl text-center font-montserrat text-dark-blue max-sm:py-2 min-sm:py-8 fading-down-slow'>
           Machine Learning | Computer Vision
         </p>
         </div>

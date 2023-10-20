e8fc021dbd4bcc9251514d7bb7dcc7a36346eb46
Ze Xian Choo <99162756+zexianchoo@users.noreply.github.com>
2023-10-20 17:15:47 -0500

    added source code

--- src/sections/ContactInfo.jsx
+++ src/sections/ContactInfo.jsx
@@ -1,4 +1,3 @@
-import { footer_icons } from '../constants'
 
 const ContactInfo = () => {
   return (
@@ -12,11 +11,14 @@ const ContactInfo = () => {
         <div className="flex flex-col space-y-4">
           <p className="info-text">
             Thank you for reading my page. </p>
-            <p className="info-text font-2xl">
-            I'm currently looking for internships and job opportunities. I am seeking an internship for Summer 2024.
+          <p className="info-text font-2xl">
+            I'm currently looking for internships and job opportunities, especially an internship for Summer 2024.
             You're always welcome to shoot me an email whenever!
           </p>
-          
+          <p className="info-text font-2xl">
+            Find the source code for this website <a href='https://github.com/zexianchoo/zexianchoo.github.io'
+            target="_blank" rel="noopener noreferrer" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" > here! </a> 
+          </p>
         </div>
 
       </div>

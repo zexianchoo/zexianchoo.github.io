3e3898eb9e7d56d2047d9f2ac22edb65bbd5e904
Sean Choo <zchoo@blizzard.com>
2025-07-23 10:29:17 +0800

    feat: update techstack

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,145 +1,141 @@
-import React from 'react'
+import React from 'react';
+
 const iconUrls = [
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/terraform/terraform-original.svg",
-        href: "https://www.terraform.io/",
-        desc: "Terraform"
-    },
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/kubernetes/kubernetes-original.svg",
-        href: "https://kubernetes.io/",
-        desc: "Kubernetes"
-    },
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
-        href: "https://www.docker.com/",
-        desc: "Docker"
-    },
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
-        href: "https://aws.amazon.com/",
-        desc: "AWS"
-    },
-    
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg",
-        href: "https://pytorch.org/",
-        desc: "pytorch"
-    },
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg",
-        href: "https://www.tensorflow.org/",
-        desc: "tensorflow"
-    },
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/opencv/opencv-original-wordmark.svg",
-        href: "https://opencv.org//",
-        desc: "OpenCV"
-    },
-
-    {
-        url: "https://huggingface.co/datasets/huggingface/brand-assets/resolve/main/hf-logo.svg",
-        href: "https://huggingface.co/",
-        desc: "HuggingFace"
-    },
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-        href: "https://react.dev/",
-        desc: "React"
-    },
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
-        href: "https://tailwindcss.com/",
-        desc: "tailwindcss"
-    },
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg",
-        href: "https://www.mongodb.com/",
-        desc: "mongoDB"
-    },
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
-        href: "https://www.mysql.com/",
-        desc: "MySQL"
-    },
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg",
-        href: "https://www.postgresql.org/",
-        desc: "PostgreSQL"
-    },
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original.svg",
-        href: "https://cloud.google.com/",
-        desc: "GCP"
-    },
-
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
-        href: "https://www.java.com/en/",
-        desc: "Java"
-    },
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
-        href: "https://angular.io/",
-        desc: "Angular"
-    },
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
-        href: "https://cplusplus.com/",
-        desc: "C++"
-    },
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain.svg",
-        href: "https://www.djangoproject.com/",
-        desc: "django"
-    },
-    {
-        url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg",
-        href: "https://flask.palletsprojects.com/en/3.0.x/",
-        desc: "flask"
-    },
-
-
-  ];
+  // DevOps & Cloud Native
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/kubernetes/kubernetes-original.svg',
+    href: 'https://kubernetes.io/',
+    desc: 'Kubernetes',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/terraform/terraform-original.svg',
+    href: 'https://www.terraform.io/',
+    desc: 'Terraform',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg',
+    href: 'https://www.docker.com/',
+    desc: 'Docker',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/helm/helm-original.svg',
+    href: 'https://helm.sh/',
+    desc: 'Helm',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/argocd/argocd-original.svg',
+    href: 'https://argo-cd.readthedocs.io/',
+    desc: 'ArgoCD',
+  },
+  {
+    url: 'https://framerusercontent.com/images/3IoQfdzvZUuQxJzxvc2ikExMapc.png?scale-down-to=512',
+    href: 'https://kargo.akuity.io/',
+    desc: 'Kargo',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jenkins/jenkins-original.svg',
+    href: 'https://www.jenkins.io/',
+    desc: 'Jenkins',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/githubactions/githubactions-original.svg',
+    href: 'https://github.com/features/actions',
+    desc: 'GH Actions',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original.svg',
+    href: 'https://about.gitlab.com/stages-devops-lifecycle/continuous-integration/',
+    desc: 'GitLab CI',
+  },
+  // Observability
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/prometheus/prometheus-original.svg',
+    href: 'https://prometheus.io/',
+    desc: 'Prometheus',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/grafana/grafana-original.svg',
+    href: 'https://grafana.com/',
+    desc: 'Grafana',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/openstack/openstack-original.svg',
+    href: 'https://www.openstack.org/',
+    desc: 'OpenStack',
+  },
+  // Machine Learning
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg',
+    href: 'https://pytorch.org/',
+    desc: 'PyTorch',
+  },
+  {
+    url: 'https://huggingface.co/datasets/huggingface/brand-assets/resolve/main/hf-logo.svg',
+    href: 'https://huggingface.co/',
+    desc: 'HuggingFace',
+  },
+  // Web & Backend
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
+    href: 'https://react.dev/',
+    desc: 'React',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg',
+    href: 'https://tailwindcss.com/',
+    desc: 'Tailwind CSS',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain.svg',
+    href: 'https://www.djangoproject.com/',
+    desc: 'Django',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg',
+    href: 'https://flask.palletsprojects.com/',
+    desc: 'Flask',
+  },
+  // Databases
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg',
+    href: 'https://www.mongodb.com/',
+    desc: 'MongoDB',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg',
+    href: 'https://www.mysql.com/',
+    desc: 'MySQL',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg',
+    href: 'https://www.postgresql.org/',
+    desc: 'PostgreSQL',
+  },
+];
 
 const TechStack = () => {
   return (
-    <div className='border-gradient-to-b border-4 border-[#fda4af]'>
+    <div className="border-4 border-[#fda4af]">
+      <p className="px-2 tech-text">My Tech Stack:</p>
 
-    <p className='px-2 tech-text'>
-        My Tech Stack:
-    </p>
-
-    <div className='py-2 px-6'>
-
-    <div className='flex flex-wrap gap-x-4 gap-y-4'>
-        {iconUrls.map((icon, index) => (
-            <div key={index} className='flex flex-col items-center w-20 max-sm:scale-75 max-sm:w-10
-                border-slate-800 border-2 border-transparent hover:bg-[#fda4af] hover:border-slate-800'>
-                <a href={icon.href} target="_blank" rel="noopener noreferrer">
-                    <img className='icon-size' src={icon.url} alt={`Icon ${index}`} />
-                </a>
-                <a href={icon.href} target="_blank" rel="noopener noreferrer">
-                    <button className="desc-text mt-2">
-                        {icon.desc}
-                    </button>
-                </a>
-            </div>
-        ))}
+      <div className="px-3 py-2">
+        <div className="flex flex-wrap justify-center gap-3">
+          {iconUrls.map((icon, index) => (
+            <a
+              key={index}
+              href={icon.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-col items-center rounded-md border-2 border-transparent p-2 hover:border-slate-800 hover:bg-[#fda4af] max-sm:w-10 max-sm:scale-75"
+            >
+              <img className="icon-size" src={icon.url} alt={icon.desc} />
+              <span className="desc-text mt-2">{icon.desc}</span>
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default TechStack
+export default TechStack;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ import { TechStack } from '../components';
 
 const Hero = () => {
   return (
-    <main className='flex flex-col max-container space-y-2'>
+    <main className='flex flex-col max-container'>
       <section
         id='home'
         className='relative sm:flex max-sm:flex-col justify-between items-center object-scale-down '
@@ -14,50 +14,50 @@ const Hero = () => {
           <img
             src={ face }
             alt='picture'
-            width={300}
-            height={300}
+            width={250}
+            height={250}
             className='rounded-full mx-auto'
           />
           <div>
-            <p className='relative max-sm:text-2xl font-semibold sm:text-3xl text-center font-montserrat text-dark-blue max-sm:py-2 min-sm:py-8 fading-down-slow'>
-              Choo Ze Xian
+            <p className='relative max-sm:text-2xl font-semibold sm:text-2xl text-center font-montserrat text-dark-blue py-4 fading-down-slow'>
+              Sean (Ze Xian) Choo 
             </p>
-            <p className='relative max-sm:text-lg max-md:xl md:text-2xl whitespace-nowrap text-center font-montserrat text-dark-blue max-sm:py-2 min-sm:py-8 fading-down-slow'>
-              Software Engineer | <br/> DevOps Engineer
+            <p className='relative text-2xl whitespace-nowrap text-center font-montserrat text-dark-blue fading-down-slow'>
+              Software Engineer | <br/> DevOps Engineer | <br/> SRE 
             </p>
           </div>
         </div>
 
         <div className='flex flex-col sm:ml-36 space-y-4 fading-down-slow'>
           <div className='py-2 sm:max-w-full'>
-            <p className='sm:text-[55px] max-sm:text-md font-montserrat text-dark-blue' >
-              Hey! 👋 My name is <span className='text-coral-red font-semibold'>Ze Xian Choo</span>. 
+            <p className='sm:text-[50px] max-sm:text-md font-montserrat text-dark-blue' >
+              Hey! 👋 My name is <span className='text-coral-red font-semibold'>Sean (Ze Xian) Choo</span>. 
             </p>
           </div>
           <div className='justify-left'>
-            <ul className='space-y-4 sm:text-lg font-light max-sm:text-sm  font-Palanquin text-dark-blue py-2' >
+            <ul className='space-y-4 sm:text-lg font-light max-sm:text-sm font-Palanquin text-dark-blue py-2' >
                 <p className='' >
-                  📖 CS Junior @ UIUC
-                  📖 CS Junior @ UIUC
+                  📖 <span className='font-semibold'>CS Senior</span> @ UIUC
                 </p>
 
                 <p className='' >
-                  ☁️ DevOps/MLOps Engineer Intern @ HTX
+                  🗄️ <span className='font-semibold'>Current SRE Intern</span> @ Blizzard Entertainment
                 </p>
 
                 <p className='' >
-                  💻 Computer Vision Researcher @ Promoting Undergraduate Research Engineering (PURE)
-                </p>
-                <p className='' >
-                  💬 Conversational Search Engineer @ PeopleWeave 
+                  💻 Incoming <span className='font-semibold'>SWE Intern</span> @ ByteDance
                 </p>
 
                 <p className='' >
-                  📈 Trading Algorithms Engineer @ Illini Blockchain
+                  💬 <span className='font-semibold'>Conversational Search Engineer</span> @ PeopleWeave 
                 </p>
 
                 <p className='' >
-                  🏘️ Sigma Phi Delta
+                  📈 <span className='font-semibold'>Trading Algorithms Engineer</span> @ Illini Blockchain
+                </p>
+
+                <p className='' >
+                  🏘️ <span className='font-semibold'>Sigma Phi Delta</span>
                 </p>
 
                 <p className='' >
